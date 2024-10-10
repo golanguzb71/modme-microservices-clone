593da215b6ad57f5a81087bafd3058e65c94386e
golanguzb71 <abdulaziomonov4@gmail.com>
2024-10-10 16:40:16 +0500

    education-update

--- education-service/migrations/migrations.go
+++ education-service/migrations/migrations.go
@@ -16,79 +16,25 @@ func readSQLFile(filepath string) (string, error) {
 }
 
 func SetUpMigrating(action string, db *sql.DB) {
-	////var sqlFile string
-	//var err error
-	//
-	//if action == "up" {
-	//	sqlFile, err = readSQLFile("migrations/education_service_up.sql")
-	//	if err != nil {
-	//		log.Fatalf("Failed to read up migration file: %v", err)
-	//	}
-	//} else if action == "down" {
-	//	sqlFile, err = readSQLFile("migrations/education_service_down.sql")
-	//	if err != nil {
-	//		log.Fatalf("Failed to read down migration file: %v", err)
-	//	}
-	//} else {
-	//	log.Println("no action")
-	//	return
-	//}
+	var sqlFile string
+	var err error
 
-	_, err := db.Exec(`
-CREATE table IF NOT EXISTS rooms
-(
-    id       serial primary key,
-    title    varchar NOT NULL,
-    capacity int     NOT NULL
-);
+	if action == "up" {
+		sqlFile, err = readSQLFile("migrations/education_service_up.sql")
+		if err != nil {
+			log.Fatalf("Failed to read up migration file: %v", err)
+		}
+	} else if action == "down" {
+		sqlFile, err = readSQLFile("migrations/education_service_down.sql")
+		if err != nil {
+			log.Fatalf("Failed to read down migration file: %v", err)
+		}
+	} else {
+		log.Println("no action")
+		return
+	}
 
-
-CREATE TABLE IF NOT EXISTS courses
-(
-    id              serial primary key,
-    title           varchar                                 NOT NULL,
-    duration_lesson int                                     NOT NULL,
-    course_duration int                                     NOT NULL,
-    price           double precision check ( price > 5000 ) NOT NULL,
-    description     text
-);
-
-
-CREATE TABLE IF NOT EXISTS groups
-(
-    id          bigserial PRIMARY KEY,
-    course_id   int                                                    NOT NULL,
-    teacher_id  uuid                                                   NOT NULL,
-    room_id     int references rooms (id),
-    date_type   varchar check (date_type in ('JUFT', 'TOQ', 'BOSHQA')) NOT NULL,
-    days        TEXT[]                                                 NOT NULL,
-    start_time  varchar                                                NOT NULL,
-    start_date  date                                                   NOT NULL,
-    end_date    date                                                   NOT NULL,
-    is_archived boolean   DEFAULT FALSE                                NOT NULL,
-    created_at  timestamp DEFAULT NOW(),
-    CONSTRAINT valid_days CHECK (array_length(days, 1) > 0 AND days <@
-                                                               ARRAY ['DUSHANBA', 'SESHANBA', 'CHORSHANBA', 'PAYSHANBA', 'JUMA', 'SHANBA', 'YAKSHANBA'])
-);
-
-CREATE TABLE IF NOT EXISTS transfer_lesson
-(
-    id            uuid PRIMARY KEY,
-    group_id      bigint references groups (id) NOT NULL,
-    real_date     date                          NOT NULL,
-    transfer_date date                          NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS group_students
-(
-    id         uuid PRIMARY KEY,
-    group_id   bigint references groups (id) NOT NULL,
-    student_id uuid                         NOT NULL,
-    condition  varchar check ( condition in ('FREEZE', 'ACTIVE', 'DELETE')) DEFAULT 'FREEZE',
-    created_at timestamp                                                   DEFAULT NOW(),
-    created_by uuid                         NOT NULL
-);
-`)
+	_, err = db.Exec(sqlFile)
 	if err != nil {
 		log.Fatalf("Failed to execute migration: %v", err)
 	}
