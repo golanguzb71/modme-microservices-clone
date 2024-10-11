9afa3ed40c026229d69321266dff127accb26584
golanguzb71 <abdulaziomonov4@gmail.com>
2024-10-11 11:56:16 +0500

    debugging_added

--- education-service/internal/repository/group_repository.go
+++ education-service/internal/repository/group_repository.go
@@ -50,7 +50,7 @@ func (r *GroupRepository) GetGroup() (*pb.GetGroupsResponse, error) {
 	}
 
 	query := `SELECT g.id, g.course_id, COALESCE(c.title, 'Unknown Course') as course_title, 
-              g.teacher_id, 'something' as teacher_name, 
+             'something' as teacher_name, 
               g.room_id, COALESCE(r.title, 'Unknown Room') as room_title,  r.capacity,
               g.date_type, g.start_time, g.start_date, g.end_date, g.is_archived, 
               g.name, COUNT(gs.id) as student_count, g.created_at
@@ -59,7 +59,6 @@ func (r *GroupRepository) GetGroup() (*pb.GetGroupsResponse, error) {
               LEFT JOIN rooms r ON g.room_id = r.id
               LEFT JOIN group_students gs ON g.id = gs.group_id
               GROUP BY g.id, c.title, r.title , r.capacity`
-
 	rows, err := r.db.Query(query)
 	if err != nil {
 		log.Printf("Error querying database: %v", err)
@@ -76,7 +75,7 @@ func (r *GroupRepository) GetGroup() (*pb.GetGroupsResponse, error) {
 		var room pb.AbsRoom
 
 		err := rows.Scan(
-			&group.Id, &course, &course.Name,
+			&group.Id, &course.Id, &course.Name,
 			&group.TeacherName, &room.Id, &room.Name, &room.Capacity,
 			&dateType, &startTime, &group.StartDate, &group.EndDate,
 			&group.IsArchived, &group.Name, &studentCount, &group.CreatedAt,
