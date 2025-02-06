7110d376b7a1b50e221df2895a833c4cdd77bb5a
abdulaziz omonov <abdulaziomonov4@gmail.com>
2025-02-06 13:09:04 +0500

    teacher id fix

--- lead-service/internal/repository/set_repository.go
+++ lead-service/internal/repository/set_repository.go
@@ -81,7 +81,7 @@ func (r *SetRepository) GetById(companyId string, setId string) (*pb.SetDataResp
 	resp := pb.SetDataResponse{}
 	err := r.db.QueryRow(`
 	SELECT  ss.title, course_id, teacher_id, date_type, days, start_time  FROM set_section ss  where id=$1 and company_id=$2
-	`, setId, companyId).Scan(&resp.Title, &resp.CourseId, &resp.TeacherName, &resp.DateType, pq.Array(&resp.Dates), &resp.LessonStartTime)
+	`, setId, companyId).Scan(&resp.Title, &resp.CourseId, &resp.TeacherId, &resp.DateType, pq.Array(&resp.Dates), &resp.LessonStartTime)
 	if err != nil {
 		return nil, err
 	}
