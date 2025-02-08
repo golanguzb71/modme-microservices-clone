fec24df48d74fd60aca6c21956e91cd0296e1e77
abdulaziz omonov <abdulaziomonov4@gmail.com>
2025-02-08 19:06:15 +0500

    teacher percent fix

--- education-service/internal/repository/attendance_repository.go
+++ education-service/internal/repository/attendance_repository.go
@@ -108,8 +108,14 @@ func (r *AttendanceRepository) CreateAttendance(ctx context.Context, companyId, 
 			isDiscounted = true
 			priceType = "PERCENT_DISCOUNT"
 		}
-		if err = utils.CalculateMoneyForLesson(r.db, &price, studentId, groupId, attendDate, discountAmount, &coursePrice, nil); err != nil {
-			return errors.New("error while getting calculate money")
+		if discountOwner == "TEACHER" {
+			if err = utils.CalculateMoneyForLesson(r.db, &price, studentId, groupId, attendDate, nil, &coursePrice, nil); err != nil {
+				return errors.New("error while getting calculate money")
+			}
+		} else {
+			if err = utils.CalculateMoneyForLesson(r.db, &price, studentId, groupId, attendDate, discountAmount, &coursePrice, nil); err != nil {
+				return errors.New("error while getting calculate money")
+			}
 		}
 	}
 	query := `
